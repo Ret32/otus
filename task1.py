3ba228576db21797c88a2efb02c91c2fc31e52e4
D.Danchenko <mkad3232@gmail.com>
2024-09-12 00:45:07 +0300

    Задания 1-5

--- task1.py
+++ task1.py
@@ -1,1 +1,17 @@
-print ('Hello')
+while True:
+
+    enter_num = input ("Введите 5-ти значное число: ")
+
+    if len(enter_num) == 5:
+        print("Введено число с верной длинной. Ваше число: ", enter_num)
+        break
+    else:
+        user_enter = input ("Введено короткое число, попробуйте еще раз. Продолжить (да/нет)? ")
+        if user_enter.lower() == 'нет':
+            print("Выход из программы")
+            break
+        elif user_enter.lower() == 'да':
+            continue
+        else:
+            print("Введено неизвестное значение. Выход из программы")
+            break
