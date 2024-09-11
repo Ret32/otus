14ec15a9832c4ce57b64e07120d92f67608686c9
D.Danchenko <mkad3232@gmail.com>
2024-09-12 01:27:05 +0300

    Задания 1-5

--- task1.py
+++ task1.py
@@ -3,7 +3,10 @@ while True:
     enter_num = input ("Введите 5-ти значное число: ")
 
     if len(enter_num) == 5:
-        print("Введено число с верной длинной. Ваше число: ", enter_num)
+        revers_num = enter_num[0:1] + enter_num[-2:0:-1] + enter_num[4:5]
+        enter_num = int(enter_num)
+        revers_num = int(revers_num)
+        print(f"Введено число с верной длинной. Ваше число: {enter_num}, реверсивное число в середине: {revers_num}")
         break
     else:
         user_enter = input ("Введено короткое число, попробуйте еще раз. Продолжить (да/нет)? ")
