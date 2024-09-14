8d0f4f30410ccb0b22a7b792b82c77a8432f4b97
D.Danchenko <mkad3232@gmail.com>
2024-09-14 12:46:53 +0300

    Управляющие конструкции

--- task2.py
+++ task2.py
@@ -5,6 +5,7 @@ match enter_task:
     case "1":
         print("\nЗадача 1: Пользователь вводит целое число, программа складывает все цифры числа,\n "
               "с полученным числом - то же самое и так до тех пор, пока не получится однозначное число.\n")
+
         enter_num = int(input("Введите число: "))
 
         while enter_num > 9:
@@ -12,5 +13,32 @@ match enter_task:
             enter_num = result
 
         print(result)
+
+    case "2":
+        pass
+
+    case "3":
+        print("\nЗадача 3: Написать упрощенную версию алгоритма RLE.\n"
+              "Алгоритм RLE объединяет подряд идущие символы в коэффициент и символ\n")
+
+        enter_text = input("Введите текст для объединения подряд идущих символов: ")
+
+        rle = []
+        symbol = enter_text[0]
+        count = 1
+
+        for elem in enter_text[1:]:
+            if elem == symbol:
+                count += 1
+            else:
+                rle.append(str(count))
+                rle.append(symbol)
+                symbol = elem
+                count = 1
+        else:
+            rle.append(str(count))
+            rle.append(symbol)
+        print("".join(rle))
+
     case _:
         print("Невернный ввод. Выход из программы")
