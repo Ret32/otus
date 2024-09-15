60cc2066e8a35c2dd0096faec138f02fb3e666f1
D.Danchenko <mkad3232@gmail.com>
2024-09-15 23:19:46 +0300

    Управляющие конструкции

--- task2.py
+++ task2.py
@@ -15,7 +15,64 @@ match enter_task:
         print(result)
 
     case "2":
-        pass
+        print("\nЗадача 2: Кинотеатр. Дан список списков, каждый вложенный список состоит из 1 и 0,\n"
+              "Количество вложенных списков - количество рядов. Пользователь вводит сколько билетов ему требуется.\n"
+              "Программа должна найти ряд, где можно приобрести нужно количество билетов (места должны быть рядом).\n"
+              "Если таких рядов несколько, то ближайший к экрану (ближайшим считается нулевой ряд).\n"
+              "Ели таких мест нет, то вывести False\n")
+
+        count_line = int(input("Введите количество рядов: "))
+        count_seats = int(input("Введите количество сидений в ряду: "))
+
+        list_line = []
+        sub_list_line = []
+        chk_enter = True
+        chk_free = True
+        a = 0
+        c = 0
+
+        for a in range(count_line):
+            list_seats = []
+            for b in range(count_seats):
+                enter_sits = int(input(f'Введите бронирование сидений в ряду {a}, № сидения - {b} (0-место свободно, 1-место занято): '))
+                if enter_sits in (0, 1):
+                    element = enter_sits
+                    list_seats.append(element)
+                else:
+                    print("Введен неверный формат бронирования сидений! Выход из программы")
+                    chk_enter = False
+                    break
+            if chk_enter:
+                list_line.append(list_seats)
+            else:
+                break
+        if len(list_line) > 0 and chk_enter:
+            print("\nСписок рядов и занятых сидений:", list_line)
+
+        if chk_enter:
+            enter_value = int(input("\nВведите количество мест для бронирования: "))
+
+            while c < len(list_line):
+                if isinstance(list_line[c], list):
+                    sub_list_line = list_line[c]
+                    d = 0
+                    while d < len(sub_list_line):
+                        if sub_list_line[d] == 0:
+                            e = 1
+                            while d + e < len(sub_list_line) and sub_list_line[d] == sub_list_line[d + e]:
+                                e += 1
+                            if e >= enter_value:
+                                print(f'Есть свободные места рядом в ряду {c} в количестве {e} шт')
+                                chk_free = False
+                            d += e
+                        else:
+                            d += 1
+                    else:
+                        c += 1
+                else:
+                    break
+            if chk_free is True:
+                print("Нет свободных рядов")
 
     case "3":
         print("\nЗадача 3: Написать упрощенную версию алгоритма RLE.\n"
