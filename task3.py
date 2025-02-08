8aafb8ef4a5728a0f066bd958c6b6e13be257fa3
D.Danchenko <mkad3232@gmail.com>
2025-02-08 17:16:25 +0300

    Функции

--- task3.py
+++ task3.py
@@ -45,5 +45,34 @@ match enter_task:
             print(convert_case("привет_мир", force_case='snake'))
             print(convert_case("ПриветМир", force_case='camel'))
             print(convert_case("ПРИВЕТ_МИР"))
+    case "2":
+            print("\nЗадача 2: Написать функцию проверяющую валидность введенной даты.\n")
+
+            from datetime import datetime
+            import re
+
+            def is_valid_date(date_string):
+
+                date_format_pattern = r'^\d{2}\.\d{2}\.\d{4}$'
+                if not re.match(date_format_pattern, date_string):
+                    print("Ошибка: Неверный формат даты. Ожидается 'DD.MM.YYYY'.")
+                    return False
+
+                try:
+
+                    date_object = datetime.strptime(date_string, '%d.%m.%Y')
+                    return date_string == date_object.strftime('%d.%m.%Y')
+                except ValueError:
+                    return False
+
+            print(is_valid_date("29.02.2000"))
+            print(is_valid_date("29.02.2001"))
+            print(is_valid_date("31.04.1962"))
+            print(is_valid_date("15.08.2023"))
+            print(is_valid_date("31.12.2023"))
+            print(is_valid_date("32.01.2023"))
+            print(is_valid_date("01.13.2023"))
+            print(is_valid_date("15-08-2023"))
+            print(is_valid_date("15082023"))
     case _:
         print("Невернный ввод. Выход из программы")
